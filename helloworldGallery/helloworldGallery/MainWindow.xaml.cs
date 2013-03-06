85e368d8ed1b03bb942fa791d4086a9facf69a93
Hannah Wei <yw.hannah@gmail.com>
2013-03-06 12:38:49 -0500

    Multitouch photo gallery using MT Vista. Project dump.

---
+++ helloworldGallery/helloworldGallery/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace helloworldTouch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Multitouch.Framework.WPF.Controls.Window
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            Photos = new ObservableCollection<string>();
+            InitializeComponent();
+        }
+
+        public ObservableCollection<string> Photos
+        {
+            get { return (ObservableCollection<string>)GetValue(PhotosProperty); }
+            set { SetValue(PhotosProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Photos.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PhotosProperty =
+            DependencyProperty.Register("Photos", typeof(ObservableCollection<string>), typeof(MainWindow));
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            foreach (string photo in Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "*.jpg").Take(5))
+            {
+                Photos.Add(photo);
+            }
+        }
+    }
+}
